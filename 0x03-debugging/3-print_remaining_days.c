56a42a2154163306dd59d6b0682611b5f275402f
Ruthatse <ruth.atse4@gmail.com>
2022-07-06 18:00:03 -0700

    3-print

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,9 @@ day++;
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-else if (month == 2 && day == 60)
+else
+{
+if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
@@ -30,5 +32,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
