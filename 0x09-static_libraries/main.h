d01b4836f1027a396052a01568ceaa48cdc4ff93
Ruthatse <ruth.atse4@gmail.com>
2022-07-22 17:04:22 -0700

    create_static_lib.sh

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: MAIN.h
- * Auth: Brennan D Baraban
+ * Auth: Victoire Vanié
  * Desc: Header file containing the prototypes of all functions
  *       included in the static library libmain.a.
  */
